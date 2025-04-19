f727a11a8a498e92ae95b95792f86a6124163d90
Seulgae <r1605866@naver.com>
2025-04-19 19:29:00 +0900

    공부내용 주석 정리

--- src/main/java/io/springbatch/springbatchseulgae/SpringBatchApplication.java
+++ src/main/java/io/springbatch/springbatchseulgae/SpringBatchApplication.java
@@ -19,15 +19,14 @@ public class SpringBatchApplication {
 // 스프링 부트 배치의 자동 설정 클래스가 실행됨으로 빈으로 등록된 모든 Job을 검색해서 초기화와 동시에 Job을 수행하도록 구성.
 
 // 스프링 배치 초기화 설정 클래스
-// 1. BatchAutoConfiguration
-// - 스프링 배치가 초기화 될 때 자동으로 실행되는 설정 클래스
-// - Job을 수행하는 JobLauncherApplicationRunner 빈을 생성
 
-// 2. SimpleBatchConfiguration
+// SimpleBatchConfiguration
 // - JobBuilderFactory와 StepBuilderFactory 생성, Job과 Step 생성용
 // - 스프링 배치의 주요 구성 요소 생성 - 프록시 객체로 생성됨.
+// Q. 프록시 객체란 : 대리인 역활을 하는 객체, 누군가 대신해서 일을 처리해주는 대리인
+// A. Proxy(대리인) -> Target(대상)
 
-// 3. BatchConfigurerConfiguration
+// BatchConfigurerConfiguration
 // a. BasicBatchConfigurer
 // - SimpleBatchConfiguration에서 생성한 프록시 객체의 실제 대상 객체를 설정하는 설정 클래스
 // - Bean으로 의존성 주입 받아서 주요 객체들을 참조해서 사용할 수 있음.
@@ -35,3 +34,7 @@ public class SpringBatchApplication {
 // b. JpaBatchConfigurer
 // - JPA 관련 객체를 생성하는 설정 클래스
 // - 사용자 정의 BatchConfigurer 인터페이스를 사용할 수 있음.
+
+// BatchAutoConfiguration
+// - 스프링 배치가 초기화 될 때 자동으로 실행되는 설정 클래스
+// - Job을 수행하는 JobLauncherApplicationRunner 빈을 생성
