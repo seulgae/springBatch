fd63ed755f9877979859ab80a091e50c9db92109
Seulgae <r1605866@naver.com>
2025-04-19 20:45:34 +0900

    helloJob 생성하기, Test

--- src/main/java/io/springbatch/springbatchseulgae/HelloJobConfiguration.java
+++ src/main/java/io/springbatch/springbatchseulgae/HelloJobConfiguration.java
@@ -13,14 +13,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor // @Autowired 의존성 주입
-@Configuration
+@Configuration // 하나의 배치 Job을 정의하고 빈 설정
 public class HelloJobConfiguration {
 
-     private final JobBuilderFactory jobBuilderFactory;
-     private final StepBuilderFactory stepBuilderFactory;
+     private final JobBuilderFactory jobBuilderFactory; // Job을 생성하는 빌더 팩토리
+     private final StepBuilderFactory stepBuilderFactory; // Step을 생성하는 빌더 팩토리
 
     @Bean
-    public Job helloJob() {
+    public Job helloJob() { // helloJob 이름으로 Job 생성
         return jobBuilderFactory.get("hello Job")
                 .start(helloStep1())
                 .next(helloStep2())
@@ -28,12 +28,15 @@ public class HelloJobConfiguration {
     }
 
     @Bean
-    public Step helloStep1() {
+    public Step helloStep1() { // helloStep 이름으로 Step생성
         return stepBuilderFactory.get("helloStep1")
-                .tasklet(new Tasklet() {
+                .tasklet(new Tasklet() { // Step 안에서 단일 태스크로 수행되는 로직 구현
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        return null;
+                        // 내용기술
+                        System.out.println("Step1 테스트");
+
+                        return RepeatStatus.FINISHED;
                     }
                 })
                 .build()
@@ -46,11 +49,16 @@ public class HelloJobConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        return null;
+                        // 내용기술
+                        System.out.println("Step2 테스트");
+
+                        return RepeatStatus.FINISHED;
                     }
                 })
                 .build()
                 ;
     }
+
+    // 최종 동작 : Job 구동 -> Step을 실행 -> Teskelt을 실행
 
 }
